366f3f7a211a704dd3cb0e4687784e92320319d7
Mario Granados <mario.alberto.granados1@gmail.com>
2023-04-14 22:41:22 -0500

    need to add folders for users db

--- src/main/java/com/art/portfolio/Controller/PostController.java
+++ src/main/java/com/art/portfolio/Controller/PostController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,26 +29,26 @@ public class PostController {
 
     private final UserRepo userRepo;
     private final PostRepo postRepo;
+    private final PasswordEncoder passwordEncoder;
 
-    public PostController(UserRepo userRepo, PostRepo postRepo) {
+    public PostController(UserRepo userRepo, PostRepo postRepo, PasswordEncoder passwordEncoder) {
         this.postRepo = postRepo;
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/post/create")
-    public String createPost(@ModelAttribute Post post, @RequestParam(name = "file") MultipartFile uploadedFile, Model model) {
+    public String createPost(@ModelAttribute Post post, @RequestParam(name = "file") MultipartFile uploadedFile,
+            Model model) {
 
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        uploadPath += user.getId().toString();
-
-
-        String filename = uploadedFile.getOriginalFilename();
-       // String filepath = Paths.get(uploadPath, filename).toString();
+        String filename = passwordEncoder.encode(uploadedFile.getOriginalFilename());
+        // String filepath = Paths.get(uploadPath, filename).toString();
         File destinationFile = new File(uploadPath, filename);
 
         File directory = new File(uploadPath);
-        if (! directory.exists()){
+        if (!directory.exists()) {
             directory.mkdirs();
             // If you require it to make the entire directory path including parents,
             // use directory.mkdirs(); here instead.
@@ -55,15 +56,13 @@ public class PostController {
 
         try {
             uploadedFile.transferTo(destinationFile);
-            System.out.println("suc");
             model.addAttribute("message", "File successfully uploaded!");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("not");
             model.addAttribute("message", "Oops! Something went wrong! " + e);
         }
         post.setUser(user);
-        post.setImageUrl(uploadPath);
+        post.setImageUrl("images/" + filename);
         postRepo.save(post);
         return "post";
     }
@@ -73,7 +72,6 @@ public class PostController {
         model.addAttribute("post", new Post());
         return "post";
     }
-
 
     /* --------------------------------- gallery -------------------------------- */
     @GetMapping("/gallery/{username}")
