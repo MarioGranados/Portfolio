2d4ec4d7d2273dc3c1a1395b41a87a92f018a184
Mario Granados <mario.alberto.granados1@gmail.com>
2023-04-22 01:32:48 -0500

    testing

---
+++ src/main/java/com/art/portfolio/Controller/TestController.java
@@ -0,0 +1,11 @@
+package com.art.portfolio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test") public String displayTest(){
+        return "test";
+    }
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,48 +1,60 @@
 const openHamburgerMenu = () => {
-    document.getElementById("main").style.marginRight = "250px";
-    document.getElementById("hamburger_menu").style.width = "250px";
-  };
-  const closeHamburgerMenu = () => {
-    document.getElementById("main").style.marginRight = "0";
-    document.getElementById("hamburger_menu").style.width = "0";
-  };
+  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("hamburger_menu").style.width = "250px";
+};
+const closeHamburgerMenu = () => {
+  document.getElementById("main").style.marginRight = "0";
+  document.getElementById("hamburger_menu").style.width = "0";
+};
 
-  const signInModalPopUp = document.getElementById("modal");
+const signInModalPopUp = document.getElementById("modal");
 
-  const openModalButton = document.getElementById("open_modal_btn");
+const openModalButton = document.getElementById("open_modal_btn");
 
-  const closeModalButton = document.getElementById("close_modal_btn");
+const closeModalButton = document.getElementById("close_modal_btn");
 
-  const navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 
-  window.onscroll = function() {
-    if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-      navbar.style.cssText = `background-color: var(--dark); transition: 300ms; height: 13vh; color: white;`;
-    } else {
-      navbar.style.cssText = `background-color: var(--light); transition: 280ms; height: 15vh; color: black;`;
-    }
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    navbar.style.cssText = `
+      background-color: var(--dark); 
+      transition: 300ms; 
+      height: 13vh; 
+      color: white;
+      box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;
+      `;
+  } else {
+    navbar.style.cssText = `background-color: var(--light); 
+    transition: 280ms; 
+    height: 15vh; 
+    color: black;`;
   }
+};
 
-  //active navbar showing current page
-  let navbarBtns = navbar.getElementsByClassName("nav_link");
-  for (let i = 0; i < navbarBtns.length; i++) {
-    navbarBtns[i].addEventListener("click", function () {
-      let current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
+//active navbar showing current page
+let navbarBtns = navbar.getElementsByClassName("nav_link");
+for (let i = 0; i < navbarBtns.length; i++) {
+  navbarBtns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// openModalButton.onclick = function () {
+//   signInModalPopUp.style.display = "block";
+//   closeHamburgerMenu();
+// };
+// closeModalButton.onclick = function () {
+//   signInModalPopUp.style.display = "none";
+// };
+//tap anywhere to close modal window
+window.onclick = function (event) {
+  if (event.target == modal) {
+    signInModalPopUp.style.display = "none";
   }
-
-  // openModalButton.onclick = function () {
-  //   signInModalPopUp.style.display = "block";
-  //   closeHamburgerMenu();
-  // };
-  // closeModalButton.onclick = function () {
-  //   signInModalPopUp.style.display = "none";
-  // };
-  //tap anywhere to close modal window
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      signInModalPopUp.style.display = "none";
-    }
-  };
+};
